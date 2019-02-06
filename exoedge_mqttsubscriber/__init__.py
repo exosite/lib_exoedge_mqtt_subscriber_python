e91c6c6931195105fb0f77a6137c6e8a08564601
Will Charlton <willcharlton@exosite.com>
2019-02-05 21:54:52 -0600

    Fixed indentation, removed cruft, added loop_stop() to end of thread.

--- exoedge_mqttsubscriber/__init__.py
+++ exoedge_mqttsubscriber/__init__.py
@@ -40,39 +40,30 @@ class MqttsubscriberExoEdgeSource(ExoEdgeSource):
             def on_message(client, userdata, msg):
                 """ Default on_message function for tunable logging. """
                 LOG.debug("userdata: {} dup: {} info: {} mid: {} payload: {} qos: {} retain: {} state: {} timestamp: {} topic: {}"
-                              .format(userdata,
-                                      msg.dup,
-                                      msg.info,
-                                      msg.mid,
-                                      msg.payload,
-                                      msg.qos,
-                                      msg.retain,
-                                      msg.state,
-                                      msg.timestamp,
-                                      msg.topic))
+                          .format(userdata,
+                                  msg.dup,
+                                  msg.info,
+                                  msg.mid,
+                                  msg.payload,
+                                  msg.qos,
+                                  msg.retain,
+                                  msg.state,
+                                  msg.timestamp,
+                                  msg.topic))
                 LOG.critical("{}->{} got {}".format(client, client.channels[msg.topic].name, msg.payload))
                 client.channels[msg.topic].put_sample(msg.payload)
             self.mqtt_clients[ip_address].on_message = on_message
             self.mqtt_clients[ip_address].connect(ip_address, port)
-
 
         for client in self.mqtt_clients.values():
             client.loop_start()
             for channel in client.channels.values():
                 client.subscribe(channel.protocol_config.app_specific_config['topic'])
 
-
         while not self.is_stopped():
             time.sleep(0.25)
+
+        for client in self.mqtt_clients.values():
+            client.loop_stop()
+
         LOG.critical("{} HAS BEEN STOPPED.".format(self.name))
-
-# set up borg instance of async source
-# src = MQTT_Subscriber().get_async_source()
-
-# # get access to the current module
-# this_module = sys.modules[__name__]
-
-# # register all desired class methods to module
-# setattr(this_module, 'subscribe', src.subscribe)
-
-# logging.critical("current module methods: {}".format(dir(sys.modules[__name__])))
